14ca78f2f79a82ccb1c71125c4af98e67d570ddd
JibinB02 <jibinbabukp02@gmail.com>
2024-04-10 02:34:05 +0530

    newwww

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd'
 
-import {Navbar,Exchanges,Homepage,Cryptocurrencies,News,CrytpoDetails} from './components';
+import {Navbar,Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails} from './components';
 import './App.css';
 
 const App = () => {
@@ -14,28 +14,28 @@ const App = () => {
         <div className='main'>
             <Layout>
                 <div className='routes'>
-                    <Switch>
-                        <Route exact path='/'>
-                            <Homepage />
-                        </Route>
-                        <Route exact path='/exchanges'>
-                            <Exchanges />
-                        </Route>
-                        <Route exact path='/cryptocurrencies'>
-                            <Cryptocurrencies />
-                        </Route>
-                        <Route exact path='/crypto/:coindId'>
-                            <CrytpoDetails />
-                        </Route>
-                        <Route exact path='/news'>
-                            <News />
-                        </Route>
-                    </Switch>
+                    <Routes>
+                        <Route path='/' element={<Homepage />} />
+                        <Route path='/exchanges' element={<Exchanges />} />
+                        <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                        <Route path='/crypto/:coindId' element={<CryptoDetails />} />
+                        <Route path='/news' element={<News />} />
+                    </Routes>
                 </div>
             </Layout>
-        </div>
-        <div className='footer'>
+        
+            <div className='footer' >
+                <Typography.Title level = {5} style={{color: 'white',textAlign: 'center'}}>
+                    CryptoVerse <br />
+                    All rights reserved
+                </Typography.Title>
 
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   );
